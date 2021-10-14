379f13ce31d42ed57336af2d82bde90c8e086b22
Klaudiusz <klaudiusz.boblack@gmail.com>
2021-10-14 12:29:28 +0200

    add RequestParam

--- src/main/java/com/example/demo/Rev_controller.java
+++ src/main/java/com/example/demo/Rev_controller.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Rev_controller {
 
     @GetMapping("/")
-    public String index() {
+    public String index(@RequestParam String rev) {
         return "Greetings from Spring Boot!";
     }
 
